2a1571a0c9ff4736eb0026edc5ad210575610131
Alessandro Desiante <Alexdesiante@gmail.com>
2025-03-30 18:00:27 +0200

    removed admin create page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -30,10 +30,10 @@
               Benvenuto {{Auth::user()->name}}
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="{{route('create.post')}}">Admin</a></li>
+              {{-- <li><a class="dropdown-item" href="{{route('create.post')}}">Admin</a></li>
               <li><hr class="dropdown-divider"></li>
               <li></li>
-              <li><hr class="dropdown-divider"></li>
+              <li><hr class="dropdown-divider"></li> --}}
               <li><a class="nav-link" href="{{route('contattaci')}}">Contattaci</a></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#" onclick="event.preventDefault(); document.querySelector('#logout').submit()">Logout</a></li>
