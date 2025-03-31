4cbb6e82a6013462397a1a9873bc184bafc33dbf
Alessandro Desiante <Alexdesiante@gmail.com>
2025-03-31 16:29:39 +0200

    task complete

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,42 +1,42 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="{{route('welcome')}}">Alecla Trip</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('welcome')}}">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('index.post')}}">Itinerari</a>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid position-fixed bg-dark text-center mt-4">
+    <a class="navbar-brand text-white titleNav" href="{{route('welcome')}}">ALECLA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="{{route('welcome')}}">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="{{route('index.post')}}">Itinerari</a>
           </li>
           @guest
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Benvenuto
             </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
+            <ul class="dropdown-menu bg-dark">
+              <li><a class="dropdown-item text-white" href="{{route('register')}}">Registrati</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
+              <li><a class="dropdown-item text-white" href="{{route('login')}}">Login</a></li>
             </ul>
           </li>
           @endguest
           @auth
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Benvenuto {{Auth::user()->name}}
             </a>
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu bg-dark">
               {{-- <li><a class="dropdown-item" href="{{route('create.post')}}">Admin</a></li>
               <li><hr class="dropdown-divider"></li>
               <li></li>
               <li><hr class="dropdown-divider"></li> --}}
-              <li><a class="nav-link" href="{{route('contattaci')}}">Contattaci</a></li>
+              <li><a class="dropdown-item text-white" href="{{route('contattaci')}}">Contattaci</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#" onclick="event.preventDefault(); document.querySelector('#logout').submit()">Logout</a></li>
+              <li><a class="dropdown-item text-white" href="#" onclick="event.preventDefault(); document.querySelector('#logout').submit()">Logout</a></li>
               <form method="POST" action="{{ route('logout')}} " id="logout">
                 @csrf
               </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,45 @@
 <x-layout title="homepage">
-    @if (session('message'))
-    <div class="alert alert-success">
-        {{ session('message') }}
-    </div>
-    @endif
-    <div class="container">
+    <header class="container-fluid text-center vh-100 ">
         <div class="row">
-            <div class="col-12">
-                <h1 class="red">Welcome</h1>
+            @if (session('message'))
+            <div class="alert alert-success mt-4 container-fluid">
+                {{ session('message') }}
+            </div>
+            @endif
+            <h1 class="title">ALECLA Viaggi</h1>
+            <div class="col-12 m-0 p-0">
+                
+                
+                {{-- CAROSELLO --}}
+                
+                {{-- <div id="carouselExampleDark" class="carousel carousel-dark slide">
+                    <div class="carousel-indicators">
+                        @foreach ($cards as $card)
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="{{$card['id']}}" class="active" aria-current="true" aria-label="{{$card['id']}}"></button>
+                        @endforeach
+                    </div>
+                    <div class="carousel-inner">
+                        @foreach ($cards as $card)
+                        <a href="{{route('paginaViaggi')}}">
+                            <div class="carousel-item @if($loop->first) active @endif" data-bs-interval="10000">
+                                <img src="{{$card['img']}}" class="d-block vh-100 w-100" alt="immagini carosello">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h2 class="text-white">{{$card['title']}}</h2>
+                                </div>
+                            </div>
+                        </a>
+                        @endforeach
+                    </div>
+                    <button class="carousel-control-prev btnWhite" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next btnWhite" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div> --}}
             </div>
         </div>
-    </div>
-    
-    
-    
-    
+    </header>
 </x-layout>
