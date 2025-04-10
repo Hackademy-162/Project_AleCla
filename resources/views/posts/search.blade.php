f15696c70bfe97b0b3c95ac62f0f43274e4169b0
Claudio <inglese.claudio@yahoo.it>
2025-04-10 15:50:38 +0200

    Front-end aggiornato 1.7

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,20 +1,19 @@
-<x-layout title="vetrina viaggi">
-    <div class="sfondoDettaglio">
-        <div class="container">
-            <div class="row justify-content-center align-content-center">
-                <div class="col-12 col-md-6 title rounded mt-5 mb-5">
-                    <h1 class="mt-5">tutti i viaggi in {{$continent->title}}</h1>
+<x-layout title="Esplorazione continentale">
+        <div class="container-fluid vh-100 sfondoDettaglioContinet">
+            <div class="row justify-content-center align-content-center text-center">
+                <div class="col-12 col-md-6 title rounded mt-5">
+                    <h1 class="titleItin">Esplorando: {{$continent->title}}</h1>
                 </div>
             </div>
-            <div class="row justify-content-around align-items-center">
+            <div class="row justify-content-around align-items-center mb-5">
                 {{-- @dd($continent->posts); --}}
                 @foreach ($continent->posts as $post)
-                <div class="col-4 mb-3">
+                <div class="col-3 mb-5">
                     <div class="card bg-dark mt-5 mb-5" style="width: 18rem;">
                         <img src="{{Storage::url($post->img)}}" class="card-img-top" alt="{{$post->title}}">
                         <div class="card-body justify-content-center align-items-center text-center">
                             <h5 class="card-title text-white text-center">{{$post->title}}:</h5>
-                            <p class="card-text mb-5 textDimension text-white">Continente:{{$post->continent->title}}</p>
+                            <p class="card-text mb-5 textDimension text-white">Continente: {{$post->continent->title}}</p>
                             <h6 class="card-title text-white text-center text-truncate">{{$post->description}}</h6>
                             <a href="{{route('show.post', ['id' => $post->id])}}" class="btn btn-light text-center">Itinerario</a>
                         </div>
@@ -23,5 +22,4 @@
                 @endforeach
             </div>
         </div>
-    </div>
 </x-layout>
